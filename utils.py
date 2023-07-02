163112a6a141e4792b0439111bf055766b5f71b9
OgnestrelYT <arssamohvalov@gmail.com>
2023-07-02 18:01:51 +0300

    First version (N)

---
+++ utils.py
@@ -0,0 +1,4 @@
+
+
+def checkCollisions(a_x, a_y, a_width, a_height, b_x, b_y, b_width, b_height):
+    return (a_x + a_width > b_x) and (a_x < b_x + b_width) and (a_y + a_height > b_y) and (a_y < b_y + b_height)
